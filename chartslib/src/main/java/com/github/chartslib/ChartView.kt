81e1e417f381328511e4161f8ec24972db963d84
Devit951 <devit951@gmail.com>
2019-08-02 10:22:59 +0500

    Made ChartView cleaner

--- chartslib/src/main/java/com/github/chartslib/ChartView.kt
+++ chartslib/src/main/java/com/github/chartslib/ChartView.kt
@@ -7,15 +7,6 @@ import android.view.View
 
 class ChartView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0): View(context, attrs, defStyle) {
 
-    private val chartPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.parseColor("#db8f75")
-    }
-
-    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.BLACK
-        textSize = 30f
-    }
-
     var data = intArrayOf()
         set(value){
             field = value
@@ -28,43 +19,21 @@ class ChartView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
             invalidate()
         }
 
+    var chart: Chart = VerticalChart()
+        set(value) {
+            field = value
+            invalidate()
+        }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (data.isNotEmpty()){
             val maxDataVal = data.max()!!
-            val widthOfChart = width / data.size
+            val sizeOfChart = chart.sizeOfChart(canvas, data.size)
             data.forEachIndexed { index, item ->
-                drawChart(widthOfChart, index, item, maxDataVal, canvas)
-                drawNumber(widthOfChart, index, item, canvas)
+                chart.draw(sizeOfChart, index, item, maxDataVal, padding, canvas)
             }
         }
-    }
-
-    private fun drawChart(widthOfChart: Int, index: Int, item: Int, maxDataVal: Int, canvas: Canvas) {
-        val bottom = height.toFloat() - height * 0.05f
-        val chartRect = RectF(
-            widthOfChart * index.toFloat() + padding,
-            height - height * (item / maxDataVal.toFloat()) + padding,
-            widthOfChart * (index.toFloat() + 1) - padding,
-            bottom - padding
-        )
-        canvas.drawRect(chartRect, chartPaint)
-    }
-
-
-    private fun drawNumber(widthOfChart: Int, index: Int, item: Int, canvas: Canvas) {
-        val x = widthOfChart * index.toFloat() + widthOfChart / 4f
-        val y = height.toFloat() - height * 0.05f / 2
-        canvas.save()
-        canvas.rotate(-45f, x + padding, y - padding)
-        canvas.drawText(
-            item.toString(),
-            x,
-            y + padding + padding / 2,
-            textPaint
-        )
-        canvas.restore()
     }
 
     private fun dp2Px(dp: Float): Float {
