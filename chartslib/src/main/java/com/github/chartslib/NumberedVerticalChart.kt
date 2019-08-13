b5305a83e7e72a330309a083ad240935b6ab72c0
Devit951 <devit951@gmail.com>
2019-08-13 11:21:39 +0500

    Centralized text bounds

--- chartslib/src/main/java/com/github/chartslib/NumberedVerticalChart.kt
+++ chartslib/src/main/java/com/github/chartslib/NumberedVerticalChart.kt
@@ -1,11 +1,8 @@
 package com.github.chartslib
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 
 class NumberedVerticalChart(private val verticalChart: VerticalChart,
-                            private val rotationDegree: Float = -45f,
                             initPaint: (Paint.() -> Unit)? = null): Chart {
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -20,17 +17,16 @@ class NumberedVerticalChart(private val verticalChart: VerticalChart,
         canvas.scale(1f, 0.95f)
         verticalChart.draw(sizeOfChart, index, item, maxDataVal, padding, canvas)
         canvas.restore()
-        val x = sizeOfChart * index.toFloat() + sizeOfChart / 4f
-        val y = height.toFloat() - height * 0.05f / 2
-        canvas.save()
-        canvas.rotate(rotationDegree, x + padding, y - padding)
+        val textBound = Rect()
+        textPaint.getTextBounds(item.toString(), 0, item.toString().length, textBound)
+        val x = sizeOfChart * (index.toFloat() + 1) - padding - (sizeOfChart / 2f) - (textBound.right / 2f)
+        val y = height.toFloat() - height * 0.05f + (height * 0.05f  / 2f)
         canvas.drawText(
             item.toString(),
             x,
-            y + padding + padding / 2,
+            y,
             textPaint
         )
-        canvas.restore()
     }
 
     override fun sizeOfChart(canvas: Canvas, totalChartCount: Int) = verticalChart.sizeOfChart(canvas, totalChartCount)
